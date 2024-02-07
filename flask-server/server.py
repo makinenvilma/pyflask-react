dfe0b5cba0ef693a35c5f363a5a450587f454d00
Vilma <vilmakinen@outlook.com>
2024-02-07 09:59:21 +0200

    Add: everything

--- flask-server/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/test')
-def test():
-    return {"test": {"test1", "test2", "test3"}}
-
-if __name__ == '__main__':
-    app.run(debug=True)
